4725a648349c8500cb58e786943e5b4fbea0158c
iamRazzakk <rakibt23p@gmail.com>
2024-10-22 08:59:38 -0400

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,18 @@ import express, { NextFunction, Request, Response } from 'express';
 import cookieParser from 'cookie-parser';
 import httpStatus from 'http-status';
 import { Routers } from './routes';
+import cors from "cors"
 const app = express();
 app.use(cookieParser());
 
 //parser
 app.use(express.json());
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }
+));
 app.use('/api/v1', Routers);
 //Testing
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
